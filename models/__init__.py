00cbc3c658ca329d868597e0dcf0d19cf6f870be
Paul Gavrikov <paul.gavrikov@hs-offenburg.de>
2022-02-17 12:31:42 +0100

    fixing robustbench training

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,11 @@
-from .lowresdensenet import lowres_densenet121, lowres_densenet161, lowres_densenet169, lowres_densenet201, lowres_densenet264
+from .lowresdensenet import lowres_densenet121, lowres_densenet161, lowres_densenet169, lowres_densenet201, \
+    lowres_densenet264
 from .lowresgooglenet import lowres_googlenet
 from .lowresinception import lowres_inception_v3
 from .lowresmobilenetv2 import lowres_mobilenet_v2
 from .lowresresnet import lowres_resnet14, lowres_resnet18, lowres_resnet18_noresidual, lowres_resnet34, \
-    lowres_resnet50, lowres_resnet101, lowres_resnet152, lowres_resnet200, lowres_resnet1202, lowres_preact_resnet14, lowres_preact_resnet18, \
+    lowres_resnet50, lowres_resnet101, lowres_resnet152, lowres_resnet200, lowres_resnet1202, lowres_preact_resnet14, \
+    lowres_preact_resnet18, \
     lowres_preact_resnet34, lowres_wide_resnet50_2, lowres_wide_resnet101_2, \
     lowres_resnext50_32x4d, lowres_resnext101_32x8d
 from .lowresvgg import lowres_vgg11_bn, lowres_vgg13_bn, lowres_vgg16_bn, lowres_vgg19_bn, lowres_vgg11, lowres_vgg13, \
@@ -11,7 +13,8 @@ from .lowresvgg import lowres_vgg11_bn, lowres_vgg13_bn, lowres_vgg16_bn, lowres
 from .lowresresnet9 import lowres_resnet9, lowres_auxresnet9
 from .lowresalexnet import lowres_alexnet
 from .lowreslenet import lowres_lenet5
-from robustbench.utils import load_model
+from robustbench.model_zoo import model_dicts as all_rb_models
+from robustbench.model_zoo.enums import BenchmarkDataset, ThreatModel
 
 all_classifiers = {
     "lowres_vgg11_bn": lowres_vgg11_bn,
@@ -54,11 +57,14 @@ all_classifiers = {
 
 
 def get_model(name, **kwargs):
-    if name.starts_with("robustbench_"):
+    if name.startswith("robustbench_"):
         dataset = kwargs["dataset"].replace("imagenet1k", "imagenet")
-        model = load_model(model_name=name.replace("robustbench_", ""), dataset=dataset, threat_model="Linf",
-                           model_dir=kwargs["robustbench_model_dir"])
-        model.reset_parameters()
+        ds_class = None
+        for k in list(BenchmarkDataset):
+            if k.value == dataset:
+                ds_class = k
+
+        model = all_rb_models[ds_class][ThreatModel.Linf][name.replace("robustbench_", "")]["model"]()
     else:
         model = all_classifiers.get(name)
     return model
--- module.py
+++ module.py
@@ -29,26 +29,24 @@ def rand_bbox(size, lam):
 class TrainModule(pl.LightningModule):
     def __init__(self, hparams):
         super().__init__()
-        
+
         self.myhparams = hparams
-        self.save_hyperparameters()    
-        
+        self.save_hyperparameters()
+
         self.criterion = torch.nn.CrossEntropyLoss()
         self.train_accuracy = Accuracy()
         self.val_accuracy = Accuracy()
         self.criterion = torch.nn.CrossEntropyLoss()
-        self.model = models.get_model(hparams["classifier"])(in_channels=hparams["in_channels"],
-                                                             num_classes=hparams["num_classes"],
-                                                             dataset=hparams["dataset"],
-                                                             robustbench_model_dir=hparams["robustbench_model_dir"])
+        self.model = models.get_model(hparams["classifier"], dataset=hparams["dataset"],
+                                      robustbench_model_dir=hparams["robustbench_model_dir"])
         self.acc_max = 0
         self.cutmix_beta = 1
-        
+
     def get_progress_bar_dict(self):
         items = super().get_progress_bar_dict()
         items["val_acc_max"] = self.acc_max
         return items
-        
+
     def forward(self, batch, metric=None):
         images, labels = batch
         if self.myhparams["aux_loss"] == 0 or not self.model.training:
@@ -74,7 +72,7 @@ class TrainModule(pl.LightningModule):
             for aux_output in aux_outputs:
                 loss += self.criterion(aux_output, labels) ** 2
             loss = torch.sqrt(loss)
-            
+
         if metric is not None:
             accuracy = metric(predictions, labels)
             return loss, accuracy * 100
@@ -85,24 +83,24 @@ class TrainModule(pl.LightningModule):
         loss, accuracy = self.forward(batch, self.train_accuracy)
         return loss
 
-    def training_epoch_end(self, outs):            
+    def training_epoch_end(self, outs):
         self.log("loss/train", np.mean([d["loss"].item() for d in outs]))
         self.log("acc/train", self.train_accuracy.compute() * 100)
         self.train_accuracy.reset()
-    
+
     def validation_step(self, batch, batch_nb):
         loss, accuracy = self.forward(batch, self.val_accuracy)
         return loss
 
     def validation_epoch_end(self, outs):
         self.log("loss/val", np.mean([d.item() for d in outs]))
-        
+
         acc = self.val_accuracy.compute() * 100
         if acc > self.acc_max:
             self.acc_max = acc.item()
-        
+
         self.log("acc_max/val", self.acc_max)
-        self.log("acc/val", acc)        
+        self.log("acc/val", acc)
         self.val_accuracy.reset()
 
     def test_step(self, batch, batch_nb):
@@ -110,17 +108,17 @@ class TrainModule(pl.LightningModule):
         self.log("acc/test", accuracy)
 
     def configure_optimizers(self):
-        
+
         if self.myhparams["freeze"] == "conv":
             for module in self.model.modules():
                 if type(module) == torch.nn.Conv2d:
                     for param in module.parameters():
                         param.requires_grad = False
-        
+
         params = filter(lambda p: p.requires_grad, self.model.parameters())
-        
+
         optimizers, schedulers = [], []
-        
+
         if self.myhparams["optimizer"] == "sgd":
             optimizers.append(torch.optim.SGD(
                 params,
